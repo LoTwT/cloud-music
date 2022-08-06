4aa25f865c474a9613efe23aa15925626e15a064
LoTwT <709937065@qq.com>
2022-08-06 13:57:42 +0800

    feat: store init

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import { IconStyle } from "./assets/iconfont/iconfont"
 import { BrowserRouter } from "react-router-dom"
 import RouterGenerator from "./routes"
 
+import { Provider } from "react-redux"
+
 import { GlobalStyle } from "./style"
 
 import "./App.css"
+import store from "./store"
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {<RouterGenerator />}
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {<RouterGenerator />}
+      </BrowserRouter>
+    </Provider>
   )
 }
 
