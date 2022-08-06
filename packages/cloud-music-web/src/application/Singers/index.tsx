0a8ee2300c915cbc3cb19afc4ca92285ee992923
LoTwT <709937065@qq.com>
2022-08-06 21:57:22 +0800

    feat: Singers item click style

--- packages/cloud-music-web/src/application/Singers/index.tsx
+++ packages/cloud-music-web/src/application/Singers/index.tsx
@@ -1,13 +1,34 @@
-import { memo } from "react"
+import { memo, useState } from "react"
 import Horizen from "../../baseUI/horizen-item"
-import { categoryTypes } from "../../api/config"
+import { categoryTypes, alphaTypes } from "../../api/config"
 import { NavContainer } from "./style"
 
 function Singers() {
+  const [category, setCategory] = useState("")
+  const [alpha, setAlpha] = useState("")
+
+  const handleUpdateAlpha = (val: string) => {
+    setAlpha(val)
+  }
+
+  const handleUpdateCatetory = (val: string) => {
+    setCategory(val)
+  }
+
   return (
     <NavContainer>
-      <Horizen list={categoryTypes} title={"分类 (默认热门):"}></Horizen>
-      {/* <Horizen list={alphaTypes} title={"首字母:"}></Horizen> */}
+      <Horizen
+        list={categoryTypes}
+        title={"分类 (默认热门):"}
+        handleClick={handleUpdateCatetory}
+        oldVal={category}
+      ></Horizen>
+      <Horizen
+        list={alphaTypes}
+        title={"首字母:"}
+        handleClick={(val) => handleUpdateAlpha(val)}
+        oldVal={alpha}
+      ></Horizen>
     </NavContainer>
   )
 }
